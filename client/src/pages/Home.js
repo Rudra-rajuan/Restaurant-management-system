572e1281da1a6922c72baf1670e1fba71a30b765
hoimonti70 <hoimontirajbonsi@gmail.com>
2024-08-23 00:08:29 +0600

    Create Home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/hero/hero";
+import Footer from "../components/footer/footer";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
